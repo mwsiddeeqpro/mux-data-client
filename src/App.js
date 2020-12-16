7b44fcc1c924d514877c2c80d1e5b71f8b0cd03f
Mohammed Siddeeq <msiddeeq@Mos-Mux.local>
2020-12-16 11:17:41 -0500

    testing mux-data api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import mux from 'mux-embed';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // Initialize Mux Data monitoring
+    if (typeof mux !== 'undefined') {
+      console.log('Mux: ', mux);
+      mux.monitor('#my-video', {
+        debug: true,
+        data: {
+          env_key: 'p93ggr3j0kbu2d924p1m0b5n3', // required
+
+          // Metadata
+          player_name: 'Little Bear', // ex: 'My Main Player'
+          player_init_time: window.muxPlayerInitTime // ex: 1451606400000
+
+          // ... and other metadata
+        }
+      });
+    }
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video id="my-video" src="https://www.w3schools.com/html/movie.mp4" controls width="500" height="400"></video>
       </header>
     </div>
   );
